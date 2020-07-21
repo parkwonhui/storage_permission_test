8a0f0d4f4f242dad78d7dceefd9da2d5d8321032
parkwonhui <parkwonhui.dev@gmail.com>
2020-07-21 22:32:52 +0900

    update: 이미지 회전

--- app/src/main/java/com/example/imagetest/MainActivity.java
+++ app/src/main/java/com/example/imagetest/MainActivity.java
@@ -1,27 +1,32 @@
 package com.example.imagetest;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-
 import android.Manifest;
+import android.content.ContentUris;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Matrix;
 import android.media.ExifInterface;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.text.TextUtils;
+import android.os.Environment;
+import android.provider.DocumentsContract;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import java.io.IOException;
-import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -41,7 +46,7 @@ public class MainActivity extends AppCompatActivity {
         imageView = findViewById(R.id.image_view);
         imageReadBtn = findViewById(R.id.image_read_btn);
         imageWriteBtn = findViewById(R.id.image_write_btn);
-        imageReadBtn.setOnClickListener(new View.OnClickListener(){
+        imageReadBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent();
@@ -51,7 +56,7 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        imageWriteBtn.setOnClickListener(new View.OnClickListener(){
+        imageWriteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
@@ -64,14 +69,10 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        Log.d("TEST_LOG", "requestCode:"+requestCode
-                                    +"resultCode:"+resultCode);
         if (requestCode == REQUEST_READ_STORAGE_IMAGE) {
             if (resultCode == RESULT_OK) {
                 try {
-
                     Bitmap image = getImage(data);
-                    Log.d("TEST_LOG", "image is null?"+(image==null));
                     if (image != null) {
                         imageView.setImageBitmap(image);
                     }
@@ -87,25 +88,26 @@ public class MainActivity extends AppCompatActivity {
 
     public Bitmap getImage(Intent intent) {
         Uri imageUri = intent.getData();
+        /*try {
+            // 기존 회전이 안되던 코드
+            InputStream in = getContentResolver().openInputStream(intent.getData());
+            Bitmap img = BitmapFactory.decodeStream(in);
+            in.close();
+            return img;
+        } catch (Exception e) {
+            return null;
+        }*/
+
+        String imagePath = getPath(this, imageUri);
         ExifInterface exifInterface = null;
         try {
-            exifInterface = new ExifInterface(imageUri.getPath());
+            exifInterface = new ExifInterface(imagePath);
         } catch (IOException e) {
             e.printStackTrace();
         }
-        // TODO : 여기서 왜 리턴을 안하죠???
         int orientation = exifInterface.getAttributeInt(ExifInterface.TAG_ORIENTATION
-                                                    , ExifInterface.ORIENTATION_UNDEFINED);
-        Bitmap img = null;
-        try {
-            InputStream in = getContentResolver().openInputStream(imageUri);
-            img = BitmapFactory.decodeStream(in);
-            in.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-            return null;
-        }
-        return getRotationBitmap(img, orientation);
+                , ExifInterface.ORIENTATION_NORMAL);
+        return getRotationBitmap(BitmapFactory.decodeFile(imagePath), orientation);
     }
 
     public void checkPhotoPermission() {
@@ -153,17 +155,17 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     public void onRequestPermissionsResult(int requestCode
-                                        , String[] permissions, int[] grantResults) {
+            , String[] permissions, int[] grantResults) {
         switch (requestCode) {
-            case REQUEST_READ_STORAGE :
+            case REQUEST_READ_STORAGE:
                 if (grantResults.length > 0
-                && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d("TEST_LOG", "Success get reading storage permission");
                 } else {
                     Log.d("TEST_LOG", "Fail get reading storage permission ");
                 }
                 break;
-            case REQUEST_WRITE_STORAGE :
+            case REQUEST_WRITE_STORAGE:
                 if (grantResults.length > 0
                         && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     Log.d("TEST_LOG", "Success get writing storage permission");
@@ -174,37 +176,18 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    // TODO : 각 CASE 마다 검증 필요
-    public static Bitmap getRotationBitmap(Bitmap bitmap, int orientation) {
-        Log.d("TEST_LOG", "orientation:"+orientation);
+    private static Bitmap getRotationBitmap(Bitmap bitmap, int orientation) {
+        Log.d("TEST_LOG", "orientation:" + orientation);
         Matrix matrix = new Matrix();
         switch (orientation) {
-            case ExifInterface.ORIENTATION_NORMAL:
-                return bitmap;
-            // 왼쪽 오른쪽 변경
-            case ExifInterface.ORIENTATION_FLIP_HORIZONTAL:
-                matrix.setScale(-1, -1);
+            case ExifInterface.ORIENTATION_ROTATE_90:
+                matrix.setRotate(90);
                 break;
             case ExifInterface.ORIENTATION_ROTATE_180:
                 matrix.setRotate(180);
                 break;
-            case ExifInterface.ORIENTATION_FLIP_VERTICAL:
-                matrix.setRotate(180);
-                matrix.postScale(-1, 1);
-                break;
-            case ExifInterface.ORIENTATION_TRANSPOSE:
-                matrix.setRotate(90);
-                matrix.postScale(-1, 1);
-                break;
-            case ExifInterface.ORIENTATION_ROTATE_90:
-                matrix.setRotate(90);
-                break;
-            case ExifInterface.ORIENTATION_TRANSVERSE:
-                matrix.setRotate(-90);
-                matrix.postScale(-1, 1);
-                break;
             case ExifInterface.ORIENTATION_ROTATE_270:
-                matrix.setRotate(-90);
+                matrix.setRotate(-270);
                 break;
             default:
                 return bitmap;
@@ -218,5 +201,137 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             return null;
         }
+    }
+
+    //https://stackoverflow.com/questions/19985286/convert-content-uri-to-actual-path-in-android-4-4/27271131#27271131
+    /**
+     * Get a file path from a Uri. This will get the the path for Storage Access
+     * Framework Documents, as well as the _data field for the MediaStore and
+     * other file-based ContentProviders.
+     *
+     * @param context The context.
+     * @param uri The Uri to query.
+     * @author paulburke
+     */
+    public static String getPath(final Context context, final Uri uri) {
+
+        final boolean isKitKat = Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT;
+
+        // DocumentProvider
+        if (isKitKat && DocumentsContract.isDocumentUri(context, uri)) {
+            // ExternalStorageProvider
+            if (isExternalStorageDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                if ("primary".equalsIgnoreCase(type)) {
+                    return Environment.getExternalStorageDirectory() + "/" + split[1];
+                }
+
+                // TODO handle non-primary volumes
+            }
+            // DownloadsProvider
+            else if (isDownloadsDocument(uri)) {
+
+                final String id = DocumentsContract.getDocumentId(uri);
+                final Uri contentUri = ContentUris.withAppendedId(
+                        Uri.parse("content://downloads/public_downloads"), Long.valueOf(id));
+
+                return getDataColumn(context, contentUri, null, null);
+            }
+            // MediaProvider
+            else if (isMediaDocument(uri)) {
+                final String docId = DocumentsContract.getDocumentId(uri);
+                final String[] split = docId.split(":");
+                final String type = split[0];
+
+                Uri contentUri = null;
+                if ("image".equals(type)) {
+                    contentUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+                } else if ("video".equals(type)) {
+                    contentUri = MediaStore.Video.Media.EXTERNAL_CONTENT_URI;
+                } else if ("audio".equals(type)) {
+                    contentUri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+                }
+
+                final String selection = "_id=?";
+                final String[] selectionArgs = new String[]{
+                        split[1]
+                };
+
+                return getDataColumn(context, contentUri, selection, selectionArgs);
+            }
+        }
+        // MediaStore (and general)
+        else if ("content".equalsIgnoreCase(uri.getScheme())) {
+            return getDataColumn(context, uri, null, null);
+        }
+        // File
+        else if ("file".equalsIgnoreCase(uri.getScheme())) {
+            return uri.getPath();
+        }
+
+        return null;
+    }
+
+    /**
+     * Get the value of the data column for this Uri. This is useful for
+     * MediaStore Uris, and other file-based ContentProviders.
+     *
+     * @param context       The context.
+     * @param uri           The Uri to query.
+     * @param selection     (Optional) Filter used in the query.
+     * @param selectionArgs (Optional) Selection arguments used in the query.
+     * @return The value of the _data column, which is typically a file path.
+     */
+    public static String getDataColumn(Context context, Uri uri, String selection,
+                                       String[] selectionArgs) {
+
+        Log.d("TEST_LOG", "context:"+context+" uri:"+uri+" selection:"+selection+" selectionArgs:"+selectionArgs.toString());
+
+        Cursor cursor = null;
+        final String column = "_data";
+        final String[] projection = {
+                column
+        };
+
+        try {
+            cursor = context.getContentResolver().query(uri, projection, selection, selectionArgs,
+                    null);
+            if (cursor != null && cursor.moveToFirst()) {
+                final int column_index = cursor.getColumnIndexOrThrow(column);
+                return cursor.getString(column_index);
+            }
+        } finally {
+            if (cursor != null)
+                cursor.close();
+        }
+        return null;
+    }
+
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is ExternalStorageProvider.
+     */
+    public static boolean isExternalStorageDocument(Uri uri) {
+        return "com.android.externalstorage.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is DownloadsProvider.
+     */
+    public static boolean isDownloadsDocument(Uri uri) {
+        return "com.android.providers.downloads.documents".equals(uri.getAuthority());
+    }
+
+    /**
+     * @param uri The Uri to check.
+     * @return Whether the Uri authority is MediaProvider.
+     */
+    public static boolean isMediaDocument(Uri uri) {
+        return "com.android.providers.media.documents".equals(uri.getAuthority());
     }
 }
